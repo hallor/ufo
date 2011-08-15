13143ac25dd772c498820564550d3380e04209b5
Mateusz Kulikowski <mateusz.kulikowski@gmail.com>
2011-08-15 21:28:53 +0200

    snapshot

--- main.cpp
+++ main.cpp
@@ -65,7 +65,92 @@ int main( int /*argc*/, char** /*args[]*/ )
     int ticks = SDL_GetTicks();
 
     SDL_FillRect(screen, NULL, 0);
+    for (int tz = 0; tz<10; tz++)
+    {
+      int sx=camera.x, sy=camera.y;
+      int ftx, fty, ltx, lty;
 
+      // First tiles to start drawing from
+      ftx = (sx + 2*sy + 16 * tz)/64 -1;
+      fty = (2*sy - sx + 16 * tz)/64 -1;
+
+      sx = camera.x + WIDTH;
+      sy = camera.y + HEIGHT;
+
+      // Last tiles to end drawing at
+      ltx = (sx + 2*sy + 16 * tz)/64 +1;
+      lty = (2*sy - sx + 16 * tz)/64 +1;
+      ltx = lty = 100;
+      ftx=fty=0;
+      //cout << "Frame" << ftx << "x" << fty << "   " << ltx << "x" << lty << endl;
+
+      //s.x=camera.x;//-64;
+      //s.y=camera.y;//-64;
+
+      for (int tx=ftx; tx<ltx; tx++)
+      {
+        s.x = camera.x + (tx * 32) + (tz*0);
+        s.y = camera.y + (tx * 16) - (tz*16);
+        for (int ty=fty; ty<lty; ty++)
+        {
+          if (tx >= 0 && tx < 100 && ty >= 0 && ty < 100)
+          {
+            Tile * t = &cm.map[tx][ty][tz];
+            if (s.x> 0 && s.y > 0 && s.x<WIDTH && s.y<HEIGHT)
+            {
+              if (t->tile && t->tile < city->count())
+                SDL_BlitSurface(city->getSprite(t->tile)->img, NULL, screen, &s);
+            }
+          }
+          s.x-=32;
+          s.y+=16;
+        }
+      }
+    }
+#if 0
+      while (sx < camera.x+WIDTH || sy < camera.y + HEIGHT)
+      {
+        tx = (sx + 2*sy + 16 * sz)/64;
+        ty = (2*sy - sx + 16 * sz)/64;
+        if (tx > 0 && tx < 100 && ty > 0 && ty < 100)
+        {
+          Tile * t = &cm.map[tx][ty][sz];
+          s.x=sx-camera.x; s.y=sy-camera.y;
+          if (t->tile)
+            SDL_BlitSurface(city->getSprite(t->tile)->img, NULL, screen, &s);
+        }
+        sx += 63;
+        if (sx > camera.x + WIDTH)
+        {
+          sy += 31;
+            if (sy > camera.y + HEIGHT)
+              break;
+          sx = 0;
+        }
+//        sx-=32;
+//        sy+=16;
+//        sx = (tx+1)*32;
+//        sy = (ty+1)*32;
+      }
+    }
+#endif
+#if 0
+    for (int sx=camera.x-50; sx<camera.x+WIDTH; sx++)
+      for (int sy=camera.y-50; sy<camera.y+HEIGHT; sy++)
+      {
+        int tx, ty;
+        tx = (sx + 2*sy + 16 * sz)/64;
+        ty = (2*sy - sx + 16 * sz)/64;
+        if (tx > 0 && tx < 100 && ty > 0 && ty < 100)
+        {
+          Tile * t = &cm.map[tx][ty][sz];
+          s.x=sx-camera.x; s.y=sy-camera.y;
+          //if (t->tile)
+            SDL_BlitSurface(city->getSprite(t->tile)->img, NULL, screen, &s);
+        }
+      }
+#endif
+#if 0
     for (int tz=0; tz<10; ++tz)
     for (int tx=0; tx<100; tx++)
     {
@@ -84,8 +169,9 @@ int main( int /*argc*/, char** /*args[]*/ )
       s.x = (tx * 32) + (tz*0);
       s.y = (tx * 16) - (tz*16);
     }
-
+#endif
     SDL_Flip(screen);
+
 
     while (SDL_PollEvent(&event))
     {
@@ -101,17 +187,20 @@ int main( int /*argc*/, char** /*args[]*/ )
     }
 
     Uint8 *keystates = SDL_GetKeyState(NULL);
-#define SPEED 1
-    if (keystates[SDLK_UP])
-      camera.y-=SPEED;
-    if (keystates[SDLK_DOWN])
-      camera.y+=SPEED;
-    if (keystates[SDLK_LEFT])
-      camera.x-=SPEED;
-    if (keystates[SDLK_RIGHT])
-      camera.x+=SPEED;
+    int speed = 5;
 
-    SDL_Delay( (1000/15) - (SDL_GetTicks() - ticks) );
+    if (keystates[SDLK_LSHIFT])
+      speed = speed * 5;
+    if (keystates[SDLK_UP])
+      camera.y+=speed;
+    if (keystates[SDLK_DOWN])
+      camera.y-=speed;
+    if (keystates[SDLK_LEFT])
+      camera.x+=speed;
+    if (keystates[SDLK_RIGHT])
+      camera.x-=speed;
+
+    SDL_Delay( (1000/30) - (SDL_GetTicks() - ticks) );
     ticks = 0;
   }
 
