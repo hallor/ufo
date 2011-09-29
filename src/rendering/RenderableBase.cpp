ae4e8eb7aae830780f89e9cad4033b45d948de63
Sethis <sethis666@gmail.com>
2011-09-29 23:11:11 +0200

    Added base classes for rendering subsystems

---
+++ src/rendering/RenderableBase.cpp
@@ -0,0 +1,56 @@
+#include "RenderableBase.h"
+
+vRenderable::vRenderable()
+{
+    // Type has to be set in constructor for every specialised clas
+    m_RenderableType = ERenderableType::Unknown;
+    
+    m_RenderingProperties = NULL;
+};
+
+vRenderable::~vRenderable()
+{
+    DeleteRenderingProperties();
+};
+
+bool vRenderable::Initialize()
+{
+    // Do not reinitialize
+    if(IsValid())
+        return true;
+
+    CreateRenderingProperties();
+
+    return IsValid();
+}
+
+void vRenderable::PrepareForRendering()
+{
+    // Nothing to be done here
+}
+
+bool vRenderable::IsValid() const
+{
+    if(!GetRenderingProperties())
+        return false;
+
+    if(!ERenderableType::IsValidType(GetRenderableType()))
+        return false;
+
+    return true;
+}
+
+// This method cannot succed as it is not possible to create properties for Unknown type
+bool vRenderable::CreateRenderingProperties()
+{   
+    return false;
+};
+
+void vRenderable::DeleteRenderingProperties()
+{
+    if(m_RenderingProperties)
+    {
+        delete m_RenderingProperties;
+        m_RenderingProperties = NULL;
+    }
+};
